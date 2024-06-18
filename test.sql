c46452c472eaeca3a2b2ac9390be157bb78f346e
LasseLuehmann <waffle669@yahoo.de>
2024-06-18 11:02:26 +0200

    LL

---
+++ test.sql
@@ -0,0 +1,13 @@
+UPDATE media SET name = 'The WITCHER III Wild Hunt' WHERE id = 23;
+UPDATE media SET name = 'Horizon: Zero Dawn' WHERE id = 24;
+UPDATE media SET name = 'ARK: Survival Evolved' WHERE id = 25;
+UPDATE media SET name = 'Stranded Deep' WHERE id = 26;
+UPDATE media SET name = 'The Forest' WHERE id = 27;
+UPDATE media SET name = 'Stellaris' WHERE id = 28;
+UPDATE media SET name = 'Grand Ages MEDIEVAL' WHERE id = 29;
+UPDATE media SET name = 'CALL of DUTY world a war' WHERE id = 30;
+UPDATE media SET name = 'HALO WARS' WHERE id = 31;
+UPDATE media SET name = 'BORDERLANDS' WHERE id = 32;
+UPDATE media SET name = 'BORDERLANDS 2' WHERE id = 33;
+UPDATE media SET name = 'BORDERLANDS 3' WHERE id = 34;
+UPDATE media SET name = 'MINECRAFT' WHERE id = 35;
